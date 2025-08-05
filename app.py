f1c791a33cf01ff4249458a4d7717b041913e28b
´BRAULIO´ <braulio_1989@hotmail.com>
2025-08-05 16:10:07 -0600

    nueva funcion para cargar datos

--- app.py
+++ app.py
@@ -7,8 +7,6 @@ import numpy as np
 import pandas as pd
 import datetime as dt
 
-
-
 app = dash.Dash(
     __name__,
     meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1"}],
@@ -21,7 +19,23 @@ app.config.suppress_callback_exceptions = True
 
 # Load data from csv
 def load_data():
-    # To do: Completar la función 
+    """
+    Carga los datos desde 'datos_energia.csv', convierte la columna 'time'
+    a formato datetime y la establece como el índice del DataFrame.
+    """
+    # Cargar el archivo .csv a un DataFrame de Pandas
+    df = pd.read_csv('datos_energia.csv')
+    
+    # Convertir la columna 'time' a formato datetime. 
+    # Asegúrate de que tu archivo CSV tenga una columna llamada 'time'.
+    # Si tiene otro nombre (ej. 'fecha'), cámbialo aquí.
+    df['time'] = pd.to_datetime(df['time'])
+    
+    # Establecer la columna 'time' como el índice del DataFrame
+    df.set_index('time', inplace=True)
+    
+    # Retornar el DataFrame procesado
+    return df
     
 
 # Cargar datos
@@ -30,7 +44,8 @@ data = load_data()
 # Graficar serie
 def plot_series(data, initial_date, proy):
     data_plot = data.loc[initial_date:]
-    data_plot = data_plot[:-(120-proy)]
+    if proy < 120:
+        data_plot = data_plot[:-(120-proy)]
     fig = go.Figure([
         go.Scatter(
             name='Demanda energética',
@@ -77,14 +92,11 @@ def plot_series(data, initial_date, proy):
             x=1
         ),
         yaxis_title='Demanda total [MW]',
-        #title='Continuous, variable value error bars',
         hovermode="x"
     )
-    #fig = px.line(data2, x='local_timestamp', y="Demanda total [MW]", markers=True, labels={"local_timestamp": "Fecha"})
     fig.update_layout(paper_bgcolor='rgba(0,0,0,0)', plot_bgcolor='rgba(0,0,0,0)', font_color="#2cfec1")
     fig.update_xaxes(showgrid=True, gridwidth=0.25, gridcolor='#7C7C7C')
     fig.update_yaxes(showgrid=True, gridwidth=0.25, gridcolor='#7C7C7C')
-    #fig.update_traces(line_color='#2cfec1')
 
     return fig
 
@@ -97,7 +109,6 @@ def description_card():
     return html.Div(
         id="description-card",
         children=[
-            #html.H5("Proyecto 1"),
             html.H3("Pronóstico de producción energética"),
             html.Div(
                 id="intro",
@@ -144,7 +155,6 @@ def generate_control_card():
                                 id="dropdown-hora-inicial-hora",
                                 options=[{"label": i, "value": i} for i in np.arange(0,25)],
                                 value=pd.to_datetime(max(data.index)-dt.timedelta(days=7)).hour,
-                                # style=dict(width='50%', display="inline-block")
                             )
                         ],
                         style=dict(width='20%')
@@ -170,8 +180,8 @@ def generate_control_card():
                         tooltip={"placement": "bottom", "always_visible": True},
                     )
                 ]
-            )     
-     
+            )    
+    
         ]
     )
 
@@ -197,8 +207,6 @@ app.layout = html.Div(
             id="right-column",
             className="eight columns",
             children=[
-
-
                 # Grafica de la serie de tiempo
                 html.Div(
                     id="model_graph",
@@ -210,8 +218,6 @@ app.layout = html.Div(
                         )
                     ],
                 ),
-
-            
             ],
         ),
     ],
@@ -226,7 +232,7 @@ app.layout = html.Div(
 )
 def update_output_div(date, hour, proy):
 
-    if ((date is not None) & (hour is not None) & (proy is not None)):
+    if ((date is not None) and (hour is not None) and (proy is not None)):
         hour = str(hour)
         minute = str(0)
 
@@ -240,4 +246,4 @@ def update_output_div(date, hour, proy):
 
 # Run the server
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run(debug=True)
